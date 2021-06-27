aa87c0618be01bf86d8dffbe21c21f03852112c4
adityacaturputra <adityacaturputra25@gmail.com>
2021-06-27 07:27:11 +0700

    change mongodb connection i will make .env later

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use("/posts", postsRoutes);
 
 const CONNECTION_URL =
-  "mongodb+srv://adityacaturputra:sqkAmfiERitQGuCT@cluster0.reduu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  "mongodb://adityacaturputra:sqkAmfiERitQGuCT@cluster0-shard-00-00.reduu.mongodb.net:27017,cluster0-shard-00-01.reduu.mongodb.net:27017,cluster0-shard-00-02.reduu.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-wthpz8-shard-0&authSource=admin&retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 mongoose
